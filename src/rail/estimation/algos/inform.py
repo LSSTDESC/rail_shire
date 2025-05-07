3428ae6cd5233b4312410331c329acf86d7e9855
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2025-05-07 15:46:26 +0200

    Improve plots a bit more

--- src/rail/estimation/algos/inform.py
+++ src/rail/estimation/algos/inform.py
@@ -600,13 +600,14 @@ class ShireInformer(CatInformer):
         pars_arr = jnp.array(self.templates_df[_DUMMY_PARS.PARAM_NAMES_FLAT])
         cdict = dict(zip(srcs, fcolors, strict=True))
         all_sfh = vmap_mean_sfr(pars_arr)
+        _min, _max = all_sfh.max()/100000, all_sfh.max()*1.1
         f, a = plt.subplots(1,1)
         for sfh, src in zip(all_sfh, self.templates_df['Dataset'], strict=True):
             a.plot(T_ARR, sfh, lw=1, ls='-', c=cdict[src])
             a.set_xlabel('Age of the Universe [Gyr]')
             a.set_ylabel('SFR '+r"$\mathrm{M_\odot.yr}^{-1}$")
             a.set_title('SFH of photo-z templates')
-            a.set_ylim(sfh.max()/100000, sfh.max()*1.1)
+            a.set_ylim(_min, _max)
             a.set_yscale('log')
 
         legs = []
