ed7468def8611b21b670790a9ec2a28af51e4826
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2025-08-06 09:02:55 +0200

    Remove obsolete files

--- tests/rail_shire/test_example_module.py
+++ tests/rail_shire/test_example_module.py
@@ -1,6 +1,4 @@
-from rail import shire
 from rail.shire import example_module
-
 
 def test_greetings() -> None:
     """Verify the output of the `greetings` function"""
@@ -12,9 +10,3 @@ def test_meaning() -> None:
     """Verify the output of the `meaning` function"""
     output = example_module.meaning()
     assert output == 42
-
-
-def test_json_to_inputs() -> None:
-    """Verify the output of the `json_to_inputs` function"""
-    output = shire.json_to_inputs('examples/conf_shire_COSMOS2020.json')
-    assert isinstance(output, dict)
